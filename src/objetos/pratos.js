cb6ea6d28589aea3da5f0a60bf6a611d4031afb1
ian2z <josefian2016@gmail.com>
2025-06-11 23:35:50 -0300

    :fix: finalizando cadastro de objetos do cardápio - resolves #6

--- src/objetos/pratos.js
+++ src/objetos/pratos.js
@@ -353,21 +353,604 @@ const pratos = [
     preco: '34,90'
   },
   {
-    id: 'jtrb',
+    id: 'jtsn',
     imagem: '/imagens/template_prato.png',
-    nome: 'Rubacão',
-    descricao: 'Feijão verde, arroz refolgado, creme de nata, coalho e carne de sol acebolada',
+    nome: 'Carne de sol na Nata',
+    descricao: 'Acompanha macaxeira, vinagre e farofa',
     categoria: 'jantar',
+    preco: '34,90'
+  },
+  {
+    id: 'jtsa',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Carne de sol Acebolada',
+    descricao: 'Acompanha macaxeira, vinagrete e farofa',
+    categoria: 'jantar',
+    preco: '34,90'
+  },
+  {
+    id: 'jtbg',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Bode guizado',
+    descricao: 'Acompanha macaxeira, vinagrete e farofa',
+    categoria: 'jantar',
+    preco: '41,90'
+  },
+  {
+    id: 'jtpf',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Frango desfiado',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '26,90'
+  },
+  {
+    id: 'jtpc',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Calabresa',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '26,90'
+  },
+  {
+    id: 'jtpn',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Carne de sol na Nata',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '29,90'
+  },
+  {
+    id: 'jtps',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Carne de sol',
+    descricao: 'Carne de sol frita ou desfiada',
+    categoria: 'jantar',
+    preco: '34,90'
+  },
+  {
+    id: 'jtpc',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Charque',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '34,90'
+  },
+  {
+    id: 'jtpp',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Purê de Picado',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '26,90'
+  },
+  {
+    id: 'ncpa',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pamonha na chapa',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '15,90'
+  },
+  {
+    id: 'ncpa',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Queijo coalho na chapa',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '13,90'
+  },
+  {
+    id: 'ncpa',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Queijo com melaço na chapa',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '16,90'
+  },
+      {
+    id: 'ncpa',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pamonha com queijo na chapa',
+    descricao: '',
+    categoria: 'jantar',
+    preco: '25,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês na chapa',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '6,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês com Ovo',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '8,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês com queijo',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '9,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês Misto',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês Americano',
+    descricao: 'Queijo, presunto, ovo e salada',
+    categoria: 'sanduiches',
+    preco: '17,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Francês e frango',
+    descricao: 'Frango desfiado e queijo',
+    categoria: 'sanduiches',
+    preco: '17,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira na chapa',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '9,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira com Ovo',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '9,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira com queijo',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira Misto',
+    descricao: '',
+    categoria: 'sanduiches',
+    preco: '16,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira Americano',
+    descricao: 'Queijo, presunto, ovo e salada',
+    categoria: 'sanduiches',
+    preco: '19,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Pão Macaxeira e frango',
+    descricao: 'Frango desfiado e queijo',
+    categoria: 'sanduiches',
+    preco: '9,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Bode Rei',
+    descricao: 'Pão de macaxeira, bode desfiado, queijo manteiga, cebola caramelizada, e chips regional',
+    categoria: 'sanduiches-especiais',
+    preco: '29,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Moranguinho do Nordeste',
+    descricao: 'Pão de macaxeira com carne de sol na nata',
+    categoria: 'sanduiches-especiais',
+    preco: '27,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Boi Quente',
+    descricao: 'Pão de macaxeira com carne de sol, quijo manteiga, cebola caramelizada e chips regional',
+    categoria: 'sanduiches-especiais',
+    preco: '36,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Galinha Inxirida',
+    descricao: 'Pão de macaxeira com frango desfiado, catupiry, mussarela, batata palha, salada e chips regional',
+    categoria: 'sanduiches-especiais',
+    preco: '36,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Mix Arretado',
+    descricao: 'Pão de macaxeira com carne de sol desfiada, frango desfiado, presunto, queijo, ovo, catupiry, batata plaha e chips regional',
+    categoria: 'sanduiches-especiais',
     preco: '39,90'
   },
   {
-    id: 'jtrb',
+    id: '',
     imagem: '/imagens/template_prato.png',
-    nome: 'Rubacão',
-    descricao: 'Feijão verde, arroz refolgado, creme de nata, coalho e carne de sol acebolada',
-    categoria: 'jantar',
+    nome: 'Queijo',
+    descricao: 'Mussarela, coalho ou manteiga',
+    categoria: 'pasteis-salgados',
+    preco: '15,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Misto',
+    descricao: 'Queijo mussarela e presunto de peru',
+    categoria: 'pasteis-salgados',
+    preco: '15,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Carne moída',
+    descricao: '',
+    categoria: 'pasteis-salgados',
+    preco: '21,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Frango',
+    descricao: 'Frango desfiado refolgado',
+    categoria: 'pasteis-salgados',
+    preco: '17,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Frango com Catupiry',
+    descricao: 'Frango desfiado refolgado com Catupiry original',
+    categoria: 'pasteis-salgados',
+    preco: '19,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Lombinho com Cream Chese',
+    descricao: '',
+    categoria: 'pasteis-salgados',
+    preco: '22,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Carde de sol',
+    descricao: 'Carne de sol desfiada com queijo',
+    categoria: 'pasteis-salgados',
+    preco: '24,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Ouro Branco',
+    descricao: '',
+    categoria: 'pasteis-doces',
+    preco: '23,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Diamente Negro',
+    descricao: '',
+    categoria: 'pasteis-doces',
+    preco: '23,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Diamente Laka',
+    descricao: '',
+    categoria: 'pasteis-doces',
+    preco: '23,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Laka Oreo',
+    descricao: '',
+    categoria: 'pasteis-doces',
+    preco: '23,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Brigadeiro de colher',
+    descricao: '150g de brigadeiro caseiro de leite Moça',
+    categoria: 'sobremesas',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Cartola tradicional',
+    descricao: 'Queijo manteiga, banana frita, açucar, Nescau, canela e leite Moça',
+    categoria: 'sobremesas',
+    preco: '24,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Cartola especial',
+    descricao: 'Queijo manteiga, banana frita, brigadeiro e morango',
+    categoria: 'sobremesas',
     preco: '39,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Cartolinha tradicional',
+    descricao: 'Queijo manteiga, banana frita, açucar, Nescau, canela e leite Moça',
+    categoria: 'sobremesas',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Cartolinha especial',
+    descricao: 'Queijo manteiga, banana frita, brigadeiro e morango',
+    categoria: 'sobremesas',
+    preco: '18,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Queijo',
+    descricao: '40g',
+    categoria: 'adicionais',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Catupiry original',
+    descricao: '50g',
+    categoria: 'adicionais',
+    preco: '7,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Presunto',
+    descricao: '40g',
+    categoria: 'adicionais',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Ovo',
+    descricao: '1 unidade',
+    categoria: 'adicionais',
+    preco: '2,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Frango desfiado',
+    descricao: '60g',
+    categoria: 'adicionais',
+    preco: '9,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Calabresa',
+    descricao: '80g',
+    categoria: 'adicionais',
+    preco: '6,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Leite Moça',
+    descricao: '50g',
+    categoria: 'adicionais',
+    preco: '3,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Banana frita',
+    descricao: '1 Unidade',
+    categoria: 'adicionais',
+    preco: '2,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Vinagrete',
+    descricao: '',
+    categoria: 'adicionais',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Farofa',
+    descricao: '',
+    categoria: 'adicionais',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Torradas',
+    descricao: '5 Unidades',
+    categoria: 'adicionais',
+    preco: '3,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Copo sem leite',
+    descricao: 'Acerola | Abacaxi com hortelã | Cajá | Caju | Graviola | Goiaba | Laranja (fruta) | Limão (fruta) | Maracujá | Morango | Manga | Mangaba | Uva',
+    categoria: 'sucos',
+    preco: '10,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Copo sem leite',
+    descricao: 'Acerola | Abacaxi com hortelã | Cajá | Caju | Graviola | Goiaba | Laranja (fruta) | Limão (fruta) | Maracujá | Morango | Manga | Mangaba | Uva',
+    categoria: 'sucos',
+    preco: '10,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Copo com leite',
+    descricao: 'Acerola | Abacaxi com hortelã | Cajá | Caju | Graviola | Goiaba | Laranja (fruta) | Limão (fruta) | Maracujá | Morango | Manga | Mangaba | Uva',
+    categoria: 'sucos',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Jarra sem leite',
+    descricao: 'Acerola | Abacaxi com hortelã | Cajá | Caju | Graviola | Goiaba | Laranja (fruta) | Limão (fruta) | Maracujá | Morango | Manga | Mangaba | Uva',
+    categoria: 'sucos',
+    preco: '20,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Café P.',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '3,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Café G.',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Café com leite P.',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '4,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Café com leite G.',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '6,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Nescau gelado.',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '10,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Capuccino',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Agua sem gás',
+    descricao: '500mg',
+    categoria: 'bebidas',
+    preco: '5,00'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Agua com gás',
+    descricao: '500mg',
+    categoria: 'bebidas',
+    preco: '5,50'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Coca KS',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '7,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Refri lata',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '8,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Refri litro',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '14,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Vitamina de açaí',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '17,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Vitamina de banana',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '12,90'
+  },
+  {
+    id: '',
+    imagem: '/imagens/template_prato.png',
+    nome: 'Long neck',
+    descricao: '',
+    categoria: 'bebidas',
+    preco: '14,90'
   }
 ];
-
 export default pratos;
