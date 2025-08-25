33da606f44cb93c460d32a8c6fba4412a1c9c031
devcomdavi <davihmn6@gmail.com>
2025-08-24 21:59:09 -0300

    migração para supabase no cardápio

--- src/pages/cardapio/index.js
+++ src/pages/cardapio/index.js
@@ -61,7 +61,7 @@ export default function Cardapio() {
                     {
                         filteredPratos?.map(prato => 
                             <div data-category={prato.categoria} class="menu-item bg-white shadow-lg rounded-2xl p-4 transition delay-100 duration-150 ease-in hover:scale-105">
-                                <img src={prato.imagem} alt={`${prato.categoria} ${prato.nome}`} class="w-full h-48 object-cover rounded-xl" />
+                                <img src={prato.imagem_url} alt={`${prato.categoria} ${prato.nome}`} class="w-full h-48 object-cover rounded-xl" />
                                 <div class="mt-4">
                                     <h2 class="font-[xilosa] text-center text-3xl font-semibold text-orange-400">{prato.nome}</h2>
                                     <p class="text-center text-gray-500 mt-2">{prato.descricao}</p>
